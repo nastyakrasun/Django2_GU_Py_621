227f3ebfdeeebfb34ee2e70af2000875c34bb714
Nastya Krasun <nastyakrasun@gmail.com>
2021-09-13 08:46:27 +0300

    прописала ключ активации в authapp/models.py

--- geekshop/authapp/models.py
+++ geekshop/authapp/models.py
@@ -4,7 +4,21 @@ from django.contrib.auth.models import AbstractUser
 
 class ShopUser(AbstractUser):
     avatar = models.ImageField(upload_to='user_avatars', blank=True)
-    age = models.PositiveIntegerField(verbose_name='возраст')
+    age = models.PositiveIntegerField(verbose_name='возраст', default=18)
 
     is_delete = models.BooleanField(default=False)
+
+    activation_key = models.CharField(
+        max_length=128,
+        blank=True,
+    )
+    activation_key_expires = models.DateTimeField(
+        default=(now() + timedelta(hours=48))
+    )
+
+    def is_activation_key_expired(self):
+        if now() <= self.activation_key_expires:
+            return False
+        else:
+            return True
 # blank=True - поля, необязательные для заполнения
