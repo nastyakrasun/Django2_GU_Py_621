637fa725329290dbd3c0baeee4cfd7a38facb501
Nastya Krasun <nastyakrasun@gmail.com>
2021-09-13 09:01:11 +0300

    контроллеры, обрабатывающие отсылку письма authapp/views.py

--- geekshop/authapp/views.py
+++ geekshop/authapp/views.py
@@ -2,8 +2,11 @@ from django.http import HttpResponseRedirect
 from django.shortcuts import render
 from django.contrib import auth
 from django.urls import reverse
+from django.conf import settings
+from django.core.mail import send_mail
 
 from authapp.forms import ShopUserLoginForm, ShopUserEditForm, ShopUserRegisterForm
+from authapp.models import ShopUser
 
 
 def login(request):
@@ -71,3 +74,30 @@ def edit(request):
     context = {'title': title, 'edit_form': edit_form}
 
     return render(request, 'authapp/edit.html', context)
+
+
+def send_verify_mail(user):
+    verify_link = reverse('auth:verify', args=[user.email, user.activation_key])
+
+    title = f'Подтверждение учётной записи {user.username}'
+
+    message = f'Для подтверждения учётной записи {user.username} на сайте {settings.DOMAIN_NAME} пройдите по ссылке: ' \
+              f'<a href="{settings.DOMAIN_NAME}{verify_link}"> Активировать </a>'
+
+    return send_mail(title, message, settings.EMAIL_HOST_USER, [user.email], fail_silently=False)
+
+
+# def verify(request, email, activation_key):
+#     try:
+#         user = ShopUser.objects.get(email=email)
+#         if user.activation_key == activation_key and not user.is_activation_key_expired():
+#             user.is_active = True
+#             user.save()
+#             auth.login(request, user)
+#             return render(request, 'authapp/verification.html')
+#         else:
+#             print(f'activation key error in user: {user.username}')
+#             return render(request, 'authapp/verification.html')
+#     except Exception as err:
+#         print(f'Error activation user: {err.args}')
+#         return HttpResponseRedirect(reverse('index'))
