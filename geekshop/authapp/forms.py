a7b4c3fcb1a612c6b2df7160f50c4460489cc83d
Nastya Krasun <nastyakrasun@gmail.com>
2021-09-13 08:50:18 +0300

    прописала метод save в authapp/forms.py

--- geekshop/authapp/forms.py
+++ geekshop/authapp/forms.py
@@ -31,6 +31,16 @@ class ShopUserRegisterForm(UserCreationForm):
 
         return data
 
+    def save(self, *args, **kwargs):
+        user = super(ShopUserRegisterForm, self).save()
+
+        user.is_active = False
+        salt = hashlib.sha1(str(random.random()).encode('utf8')).hexdigest()[:6]
+        user.activation_key = hashlib.sha1((user.email + salt).encode('utf8')).hexdigest()
+        user.save()
+
+        return user
+
 class ShopUserEditForm(UserChangeForm):
     class Meta:
         model = ShopUser
