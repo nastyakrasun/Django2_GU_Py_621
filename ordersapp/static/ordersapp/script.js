bfcaa9cf568fc5a3fbdbc5f1d1f6db46dcf471f9
Nastya Krasun <nastyakrasun@gmail.com>
2021-09-24 14:52:54 +0300

    внесли изменения в ordersapp/static/script.js для работы ajax

--- ordersapp/static/ordersapp/script.js
+++ ordersapp/static/ordersapp/script.js
@@ -33,16 +33,6 @@ window.onload = function () {
             orderSummaryUpdate(price_arr[orderitem_num], delta_quantity);
         }
     });
-    $('.order_form').on('click', 'input[type="checkbox"]', function () {
-       var target = event.target;
-       orderitem_num = parseInt(target.name.replace('orderitems-', '').replace('-DELETE', ''));
-       if (target.checked) {
-           delta_quantity = -quantity_arr[orderitem_num];
-       } else {
-           delta_quantity = quantity_arr[orderitem_num];
-       }
-       orderSummaryUpdate(price_arr[orderitem_num], delta_quantity);
-    });
     function orderSummaryUpdate(orderitem_price, delta_quantity) {
         delta_cost = orderitem_price * delta_quantity;
         order_total_price = Number((order_total_price + delta_cost).toFixed(2));
@@ -65,5 +55,44 @@ window.onload = function () {
         prefix: 'orderitems',
         remove: deleteOrderItem,
     });
+    if (!order_total_quantity) {
+      orderSummaryRecalc();
+  }
+  function orderSummaryRecalc() {
+      order_total_quantity = 0;
+      order_total_price = 0;
+      total_forms = parseInt($('input[name=orderitems-TOTAL_FORMS]').val());
+      for (var i = 0; i < total_forms; i++) {
+          order_total_quantity += quantity_arr[i];
+          order_total_price += quantity_arr[i] * price_arr[i];
+      }
+      $('.order_total_quantity').html(order_total_quantity.toString());
+      $('.order_total_cost').html(Number(order_total_price.toFixed(2).toString()));
+  }
+  $('.order_form').on("change", "select", function () {
+      var target = event.target;
+      orderitem_num = parseInt(target.name.replace('orderitems-', '').replace('-product', ''));
+      var orderitem_product_pk = target.options[target.selectedIndex].value;
+      if (orderitem_product_pk) {
+          $.ajax({
+              url: '/order/product/' + orderitem_product_pk + '/price/',
+              success: function (data) {
+                  if (data.price) {
+                      price_arr[orderitem_num] = parseFloat(data.price);
+                      if (isNaN(quantity_arr[orderitem_num])) {
+                          quantity_arr[orderitem_num] = 0;
+                      }
+                      var price_html = '<span>' +  data.price.toString().replace('.', ',') +'</span> руб.';
+                      var current_tr = $('.order_form table').find('tr:eq(' + (orderitem_num + 1) + ')');
+                      current_tr.find('td:eq(2)').html(price_html);
+                      if (isNaN(current_tr.find('input[type="number"]').val())) {
+                          current_tr.find('input[type="number"]').val(0);
+                      }
+                      orderSummaryRecalc();
+                  }
+              }
+          });
+      }
+  });
 
 }
